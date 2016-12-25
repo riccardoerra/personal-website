6772b19c00bcc754f00180d25e9490af4c088e05
Riccardo <rierra@cisco.com>
2016-12-25 01:16:05 +0100

    old-version-2012: Adding 2012 version of personal website
    Change-Id: Ia977ea76184be6d1ee8351c5b1cf64f1f0cc61a7

---
+++ js/global.js
@@ -0,0 +1,8 @@
+// When ready...
+window.addEventListener("load",function() {
+  // Set a timeout...
+  setTimeout(function(){
+    // Hide the address bar!
+    window.scrollTo(0, 1);
+  }, 0);
+});
